a5c7edf12ebd4d32f722d598a6b4d7aad154a6dd
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-07-30 16:51:31 +0300

    update solutions:

--- VB/Chart Histogram WPF/MainWindow.xaml.vb
+++ VB/Chart Histogram WPF/MainWindow.xaml.vb
@@ -1,18 +1,6 @@
-﻿Imports DevExpress.Xpf.Core
-Imports System
-Imports System.Collections.Generic
-Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
+﻿Imports System.Collections.ObjectModel
+Imports DevExpress.Xpf.Core
 Imports System.Windows
-Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace Chart_Histogram_WPF
 	''' <summary>
