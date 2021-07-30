b113c6e6b9c2b1afdd1c0d45d7b2ab657fe52dc6
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-07-30 13:57:01 +0300

    Add CS solution

---
+++ CS/Chart Histogram WPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chart_Histogram_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+    public class ViewModel
+    {
+        public ObservableCollection<Point> PointsCollection { get; private set; }
+
+        public ViewModel()
+        {
+            PointsCollection = CreatePointsCollection(20);
+        }
+        ObservableCollection<Point> CreatePointsCollection(int count)
+        {
+            ObservableCollection<Point> seriesData = new ObservableCollection<Point>();
+            Random random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int x = random.Next(1, 20);
+                int y = random.Next(1, 20);
+                seriesData.Add(new Point(x, y));
+            }
+            return seriesData;
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,17 @@
+*Files to look at*:
+- [MainWindow.xaml.cs](./CS/MainWindow.xaml.cs) ([MainWindow.xaml.vb](./VB/MainWindow.xaml.vb))
+- [MainWindow.xaml](./CS/MainWindow.xaml)
+
+# How to Add a Line Series to a Histogram Chart
+
+The following example shows how to plot a common XY series with a histogram in the same chart:
+
+![](images/histogram-chart.png)
+
+1. Create and configure histogram and line series.
+2. Add a `ViewModel`and assign it to the [](xref:System.Windows.FrameworkElement.DataContext) property. 
+2. Use the [Series.DataSource](https://docs.devexpress.com/WPF/DevExpress.Xpf.Charts.Series.DataSource) property to bind series to an an observable collection from the `ViewModel`.
+
+## Documentation
+
+- [Histogram](https://docs.devexpress.com/WPF/400974/controls-and-libraries/charts-suite/chart-control/histogram?p=netframework)
